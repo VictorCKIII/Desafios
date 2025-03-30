3cb480e717017917a7dade33b57b608278a9b9a8
Victor <eduardovictor822@gmail.com>
2025-03-30 15:41:39 -0300

    Finalizado v2

---
+++ main.py
@@ -0,0 +1,19 @@
+
+from Funções import retorna_links
+from Funções import baixa_pdfs
+from Funções import compacta_pdf
+
+
+recebe_link = retorna_links.retorna_links()
+
+baixa_pdfs.baixa_pdf(recebe_link)
+
+compacta_pdf.compacta()
+
+
+   
+
+
+
+
+    
